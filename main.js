fcd7967961b30acb5a9774b98e15c6beeb44ddac
OlgaEva <olga.rosas@gmail.com>
2020-02-24 16:28:28 -0400

    add styling

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
     const mainContainer = document.getElementById("main-container")
-    console.log("DOMContentLoaded")
+    // console.log("DOMContentLoaded")
   })
-
 
 const button = document.getElementById("button");
 button.addEventListener("click", getRandom) 
 
 function getRandom(){
+    if(document.getElementById("refran-li")){
+        document.getElementById("refran-li").remove()
 
-    let randomEl = REFRANES_ARRAY[Math.floor(Math.random() * REFRANES_ARRAY.length)]
+        let randomEl = refranes_array[Math.floor(Math.random() * refranes_array.length)]
 
-    let refranLi = document.createElement("li")
-    refranLi.innerText = randomEl
+        let refranLi = document.createElement("li")
+        refranLi.id = "refran-li"
+        refranLi.innerText = randomEl
 
-    const refranesDiv = document.getElementById("refran")
-    refranesDiv.append(refranLi)
+        const refranesDiv = document.getElementById("refran")
+        refranesDiv.append(refranLi)
 
+    } else{
+
+        let randomEl = refranes_array[Math.floor(Math.random() * refranes_array.length)]
+
+        let refranLi = document.createElement("li")
+        refranLi.id = "refran-li"
+        refranLi.innerText = randomEl
+
+        const refranesDiv = document.getElementById("refran")
+        refranesDiv.append(refranLi)
+    }
 };
 
